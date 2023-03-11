1c2f19bb35e8dc23c800424cb46018018a52fe0f
Ricardo De Paz <ricar.arturo@gmail.com>
2023-03-11 17:24:07 -0600

    Página de Entradas y Salidas modificada

--- registroEntrada.php
+++ registroEntrada.php
@@ -1,31 +1,40 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 
-<?php 
-		require('header.php');
+	<?php
+	require('header.php');
 	?>
 
 	<div class="container mt-4">
 		<h1 class="text-center mb-4">Registro de Entrada</h1>
-		<form>
+		<form action="" method="POST" >
 			<div class="form-group">
-				<label for="concepto">Concepto:</label>
-				<input type="text" class="form-control" id="concepto">
-			</div>
-			<br><br>
+				<label for="tipoEntrada">Tipo:</label>
+				<select class="form-select" id="tipoEntrada" required>
+				</select>
+			</div><br>
+			
 			<div class="form-group">
 				<label for="cantidad">Cantidad:</label>
-				$<input type="number" class="form-control" id="cantidad">
-			</div>
-			<br>
+				$<input type="number" class="form-control" id="cantidad" required>
+			</div><br>
+			
+
+			<div class="form-group">
+				<label for="factura">Imagen de Factura:</label>
+				<input class="form-control" accept=".jpg, .jpeg, .png, .webp, .gif" type="file" name="archivo">
+			</div><br>
+
+			
 			<button type="submit" class="btn btn-primary">Registrar Entrada</button>
 		</form>
 	</div>
 
-	
+
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</body>
+	</body>
+
 </html>
