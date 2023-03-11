92dd2970937fbefa5b571708f7b52bcef06bd97c
Ricardo De Paz <ricar.arturo@gmail.com>
2023-03-11 16:25:22 -0600

    Login Creado

--- login.php
+++ login.php
@@ -23,7 +23,7 @@
                         <form>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Correo Electrónico:</label>
-                                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+                                <input type="email" class="form-control" id="email"/>
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label">Contraseña:</label>
