9729264770d7b1b4ee8deff9cd53112f9561774b
Ricardo De Paz <ricar.arturo@gmail.com>
2023-03-11 22:45:12 -0600

    Registro de Usuarios Completo

--- model/registroUsuario.php
+++ model/registroUsuario.php
@@ -11,10 +11,21 @@ if(isset($_POST['email']) and isset($_POST['password']) and isset($_POST['passwo
     }
 
     else{ 
-        $clave = password_hash($password1,PASSWORD_DEFAULT);
-        $sentencia = $bd->prepare("INSERT INTO usuarios(emailUsuario,claveUsuario) VALUES (?,?);");
-        $resultado = $sentencia->execute([$email,$clave]);
-        header('location:../login.php?registro=1');
+        $comprobarUsuario = $bd->prepare("SELECT * from usuarios where emailUsuario = ?;");
+        $comprobarUsuario->execute([$email]);
+
+        $usuario= $comprobarUsuario->fetch(PDO::FETCH_OBJ);
+
+        if($usuario == true){
+            header('location:../registro.php?error=2');
+        }
+
+        else{
+            $clave = password_hash($password1,PASSWORD_DEFAULT);
+            $sentencia = $bd->prepare("INSERT INTO usuarios(emailUsuario,claveUsuario) VALUES (?,?);");
+            $resultado = $sentencia->execute([$email,$clave]);
+            header('location:../login.php?registro=1');
+        }       
     }
 }
 ?>
