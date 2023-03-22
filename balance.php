697cbced807ed13d60e45346e2492f72c7ccd829
Kelvin <88606122+kelvinjuarez@users.noreply.github.com>
2023-03-21 20:36:24 -0600

    Cambios modulo Balance
    Cambios

--- balance.php
+++ balance.php
@@ -10,41 +10,6 @@
 	require('model/piechart.php');
 	?>
 
-	<div class="container mt-4">
-		<h1 class="text-center mb-4">Balance de Entradas y Salidas</h1>
-		<table class="table table-striped">
-			<thead>
-				<tr>
-					<th scope="col">Tipo</th>
-					<th scope="col">Cantidad</th>
-				</tr>
-			</thead>
-			<tbody>
-				<!-- Aquí iría el código PHP para mostrar el balance -->
-				<tr>
-					<td>Entradas</td>
-					<td>$1000</td>
-				</tr>
-				<tr>
-					<td>Salidas</td>
-					<td>$500</td>
-				</tr>
-				<tr>
-					<td>Balance</td>
-					<td>$500</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-
-	<?php
-	require('template/footer.php')
-	?>
-
-	</body>
-
-</html>
-=======
 </head>
 <body>
 
@@ -151,4 +116,3 @@
 ?>
 </body>
 </html>
->>>>>>> Stashed changes
